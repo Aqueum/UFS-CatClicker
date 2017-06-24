48d9ef75b057df4608295147ec3f3d77f257c3cc
Aqueum <martin@aqueum.com>
2017-06-24 13:34:08 +0100

    Move cat object definitions into cats array

--- js/script.js
+++ js/script.js
@@ -14,28 +14,28 @@ function Cat(nom, imgURL, credit, creditURL) {
         '<br/>'+'<p>'+this.clicks+' clicks</p>'};
 }
 
-var cD9T = new Cat("D9T",
+var cats = [
+    cD9T = new Cat("D9T",
     "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/CatD9T.jpg/1024px-CatD9T.jpg",
     "By Shaun Greiner (CAT D9T) [CC BY-SA 2.0 (http://creativecommons.org/licenses/by-sa/2.0)], via Wikimedia Commons",
-    "https://commons.wikimedia.org/wiki/File%3ACatD9T.jpg");
-var c777D = new Cat("777D",
+    "https://commons.wikimedia.org/wiki/File%3ACatD9T.jpg"),
+    c777D = new Cat("777D",
     "https://upload.wikimedia.org/wikipedia/commons/1/15/Caterpillar_777D.jpg",
     "By Jo Mateix (Own work) [Public domain], via Wikimedia Commons",
-    "https://commons.wikimedia.org/wiki/File%3ACaterpillar_777D.jpg");
-var c323D = new Cat("323D",
+    "https://commons.wikimedia.org/wiki/File%3ACaterpillar_777D.jpg"),
+    c323D = new Cat("323D",
     "https://upload.wikimedia.org/wikipedia/commons/4/40/Sz%C3%A9ll_K%C3%A1lm%C3%A1n_t%C3%A9r_-_2015.04.10_%281%29.JPG",
     "By Derzsi Elekes Andor (Own work) [CC BY-SA 4.0 (http://creativecommons.org/licenses/by-sa/4.0)], via Wikimedia Commons",
-    "https://commons.wikimedia.org/wiki/File%3ASz%C3%A9ll_K%C3%A1lm%C3%A1n_t%C3%A9r_-_2015.04.10_(1).JPG");
-var c350D = new Cat("350D",
+    "https://commons.wikimedia.org/wiki/File%3ASz%C3%A9ll_K%C3%A1lm%C3%A1n_t%C3%A9r_-_2015.04.10_(1).JPG"),
+    c350D = new Cat("350D",
     "https://upload.wikimedia.org/wikipedia/commons/d/d1/Caterpillar_D350D.jpg",
     "By Bidgee (Own work) [CC BY 3.0 (http://creativecommons.org/licenses/by/3.0)], via Wikimedia Commons",
-    "https://commons.wikimedia.org/wiki/File%3ACaterpillar_D350D.jpg");
-var c365B = new Cat("365B",
+    "https://commons.wikimedia.org/wiki/File%3ACaterpillar_D350D.jpg"),
+    c365B = new Cat("365B",
     "https://upload.wikimedia.org/wikipedia/commons/1/1b/Hydraulicke_demolicni_nuzky_na_podvozku_CAT_330.jpg",
     "By Michal Maňas [CC BY-SA 2.5 (http://creativecommons.org/licenses/by-sa/2.5)], via Wikimedia Commons",
-    "https://commons.wikimedia.org/wiki/File%3AHydraulicke_demolicni_nuzky_na_podvozku_CAT_330.jpg");
-
-var cats = [cD9T, c323D, c350D, c365B, c777D];
+    "https://commons.wikimedia.org/wiki/File%3AHydraulicke_demolicni_nuzky_na_podvozku_CAT_330.jpg")
+];
 
 for (var i in cats) {
     var elem = document.createElement('div');
